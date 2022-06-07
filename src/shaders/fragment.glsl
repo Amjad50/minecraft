cd8828e90e58bd30895658e67589790792ec180b
Amjad Alsharafi <amjadsharafi10@gmail.com>
2022-06-07 17:13:24 +0300

    Added ambient light, so that unlit objects don't become black

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,9 +6,10 @@ layout(location = 1) in  vec3 v_normal;
 layout(location = 0) out vec4 f_color;
 
 const vec3 DIRECTION_TO_LIGHT = normalize(vec3(1.0, 3.0, -2.0));
+const float AMBIENT_LIGHT = 0.2;
 
 void main() {
     float light_intensity = max(dot(normalize(v_normal), DIRECTION_TO_LIGHT), 0);
 
-    f_color = light_intensity * v_color;
+    f_color = vec4(v_color.rgb * (light_intensity + AMBIENT_LIGHT), v_color.a);
 }
